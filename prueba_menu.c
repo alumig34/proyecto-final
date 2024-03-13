6ce7380807bed6e4fa53a72b035f0385dd1aef11
euyin30 <euyin30@gmail.com>
2024-03-13 21:23:02 +0100

    prueba_menu 1

---
+++ prueba_menu.c
@@ -0,0 +1,24 @@
+#include "Menu.h"
+
+int main() {
+
+    int opcion;
+
+    COORD c;
+    gotoxy_printf(40,0,"EMPEZAMOS ...");
+    c = GetCursorPosition();
+    printf("x=%d, y=%d\n", c.X, c.Y);
+
+    char *aMenu[] = {"uno","dos","tres","Salir"};
+    opcion = menu((COORD){10,1}, aMenu, sizeof(aMenu)/sizeof(aMenu[0]));
+    gotoxy(11,1);  printf("OPCION=====>%d\n", opcion);
+
+    gotoxy_printf(40,2,"TERMINAMOS...");
+    c = GetCursorPosition();
+    printf("x=%d, y=%d\n", c.X, c.Y);
+
+    gotoxy(0,10);
+    //system("PAUSE");
+
+    return 0;
+}
