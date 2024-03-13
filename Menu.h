6c8e95fd4f4e18fe0ea3b8327600bab3ec2863a4
euyin30 <euyin30@gmail.com>
2024-03-13 14:30:45 +0100

    commit Menú (1º)

---
+++ Menu.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+#include "funciones.h"
+
+int menu(COORD, char *[], int);
+
+#endif // MENU_H_INCLUDED
